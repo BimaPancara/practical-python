461a51182b66ed20d6b4dad8527ac488eb926cce
BimaPancara <bimapancara0@gmail.com>
2023-04-28 15:07:19 +0700

    Add function and use csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
 
-read_portfolio = open('Data/portfolio.csv', 'rt')
+def portfolio_cost(filename):
+    import csv
+    read_portfolio = open('Data/portfolio.csv')
 
-total_cost = 0.0
+    total_cost = 0.0
     
-with read_portfolio as read:
-    headers = next(read_portfolio)
-    for line in read:
-        row = line.split(',')
-        shares = int(row[1])
-        price = float(row[2])
-        total_cost += shares*price
+    with read_portfolio as read:
+        rows = csv.reader(read_portfolio)
+        headers = next(rows)
+        for row in rows:
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost += shares*price
             
-    print(total_cost)
+        return total_cost
+
+if len(sys.argv) ==2:
+    filename = sys.argv[1]
+else:
+    filename='Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total Cost:', cost)
