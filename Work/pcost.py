ed254cd5fa175889d46a36caa080eed02532c770
BimaPancara <bimapancara0@gmail.com>
2023-04-28 13:51:22 +0700

    Added Answer for Excercise 1.27, Total Cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+read_portfolio = open('Data/portfolio.csv', 'rt')
+
+total_cost = 0.0
+    
+with read_portfolio as read:
+    headers = next(read_portfolio)
+    for line in read:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2])
+        total_cost += shares*price
+            
+    print(total_cost)
